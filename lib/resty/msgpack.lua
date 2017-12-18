2937c7657a8455cd800c6d9167fb3013d22feeb3
chronolaw <chrono_cpp@me.com>
2017-12-18 14:33:19 +0800

    code clean

--- lib/resty/msgpack.lua
+++ lib/resty/msgpack.lua
@@ -1,10 +1,12 @@
+-- Copyright (C) chronolaw
+
 local mp = require "resty.MessagePack"
-local ffi_mp = require "resty.luajit-msgpack-pure"
+local ffi = require "resty.luajit-msgpack-pure"
 
 return setmetatable(
     {
         version = "0.0.2",
-        ffi = ffi_mp
+        ffi = ffi
     },
 
     {__index = mp}
